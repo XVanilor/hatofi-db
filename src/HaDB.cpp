9a5f43f76799ddec51626ec2f752b042daf445ba
XVanilor <vanilor.dev@gmail.com>
2023-01-12 20:40:16 +0100

    Added query feature

--- src/HaDB.cpp
+++ src/HaDB.cpp
@@ -3,10 +3,14 @@
 //
 
 #include <filesystem>
+#include <fmt/core.h>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
-#include <fmt/core.h>
+#include <utility>
+#include "lib/cpp-base64/base64.h"
+#include "lib/crypto/md5.h"
+#include "lib/uuid_v4.h"
 
 #include "HaUtils.h"
 #include "HaDB.h"
@@ -99,14 +103,110 @@ void HaDB::load(const std::string& file)
         // Data splitting
         std::vector<std::string> columns = tokenize(line, ":");
 
-        std::string dataOutFileName = fmt::format("{}/dataclass/{}/{}/{}/{}.md5", this->getRoot(), columns[1], columns[3].substr(0,2), columns[3].substr(2,2),columns[3]);
+        std::string dataOutFileName = fmt::format("{}/dataclass/{}/{}/{}/{}.crypto", this->getRoot(), columns[1], columns[3].substr(0,2), columns[3].substr(2,2),columns[3]);
         dataOutFile.open(dataOutFileName);
         dataOutFile << fmt::format("b64:{}\n", columns[2]);
         dataOutFile.close();
 
-        std::string entityOutFileName = fmt::format("{}/entity/{}/{}/{}.md5", this->getRoot(), columns[0].substr(0,2), columns[0].substr(2,2),columns[0]);
+        std::string entityOutFileName = fmt::format("{}/entity/{}/{}/{}.crypto", this->getRoot(), columns[0].substr(0,2), columns[0].substr(2,2),columns[0]);
         entityOutFile.open(entityOutFileName, std::ios_base::app); // append instead of overwrite
         entityOutFile << fmt::format("{} {} {}\n", columns[3], curDate, leakUuid);
         entityOutFile.close();
     }
+}
+
+std::string HaDB::query(std::string dataclass, std::string searchString, HaDB::MATCH_TYPE matchType)
+{
+    if(searchString.length() < MIN_SEARCH_LEN)
+        throw std::invalid_argument("Search must be at least "+std::to_string(MIN_SEARCH_LEN)+" characters long");
+
+    std::string searchPath = this->getRoot() + "/dataclass/"+dataclass;
+
+    UUIDv4::UUIDGenerator<std::mt19937_64> uuidGenerator;
+    std::string task_uuid = uuidGenerator.getUUID().str();
+
+    if(matchType == HaDB::MATCH_TYPE::PARTIAL)
+    {
+        // Encode string to base64 and calculate possible variants
+        // std::string is base64 encoded string, first int is string length, second int is the length of prefix
+        std::string b64_0 = base64_encode(searchString);
+        std::string b64_1 = base64_encode('a'+searchString);
+        std::string b64_2 = base64_encode("aa"+searchString);
+
+        int searchString_mod = 8*searchString.length() % 6;
+
+        // Remove suffix and keep only common for all subvariations
+        if(searchString_mod == 0)
+        {
+            // b64_0 = b64_0; Useless but keep it for better reading comprehension
+            b64_1.erase(b64_1.length()-3,3);
+            b64_2.erase(b64_2.length()-2,2);
+        }
+        if(searchString_mod == 2)
+        {
+            b64_0.erase(b64_0.length() - 3, 3);
+            b64_1.erase(b64_1.length()-2,2);
+            //b64_2 = b64_2;
+        }
+        if(searchString_mod == 4)
+        {
+            b64_0.erase(b64_0.length() - 2, 2);
+            //b64_1 = b64_1
+            b64_2.erase(b64_2.length() - 3, 3);
+        }
+
+        // Remove prefix for variant 1 & 2
+        b64_1.erase(0,2);
+        b64_2.erase(0,3);
+
+        // Starts search
+        std::string cmd0;
+        std::string cmd1;
+        std::string cmd2;
+
+        // Check if sift was installed on system. If not, use grep
+        if(system("which sift > /dev/null 2>&1"))
+        {
+            // sift is not installed. Running search using grep and send the ticket number to output
+            cmd0 = "grep -r '"+b64_0+"' '"+searchPath+"' > /tmp/"+task_uuid+".0.txt &";
+            cmd1 = "grep -r '"+b64_1+"' '"+searchPath+"' > /tmp/"+task_uuid+".1.txt &";
+            cmd2 = "grep -r '"+b64_2+"' '"+searchPath+"' > /tmp/"+task_uuid+".2.txt &";
+        }
+        // sift is installed
+        else
+        {
+            cmd0 = "sift -r '"+b64_0+"' '"+searchPath+"' > /tmp/"+task_uuid+".0.txt &";
+            cmd1 = "sift -r '"+b64_1+"' '"+searchPath+"' > /tmp/"+task_uuid+".1.txt &";
+            cmd2 = "sift -r '"+b64_2+"' '"+searchPath+"' > /tmp/"+task_uuid+".2.txt &";
+        }
+
+        system(cmd0.c_str());
+        system(cmd1.c_str());
+        system(cmd2.c_str());
+    }
+    // Performs exact and full match search using MD5 hash
+    else
+    {
+        // Get first 2 and 4th bytes of MD5 hash
+        std::string searchHash = md5(std::move(searchString));
+
+        std::string root1 = searchHash.substr(0,2);
+        std::string root2 = searchHash.substr(2,2);
+
+        // File path
+        std::string fPath = searchPath + "/" + root1 + "/" + root2 + "/" + searchHash + ".md5";
+        std::string resPath = "/tmp/"+task_uuid+".0.txt";
+
+        std::string cmd0 = "cat '"+fPath+"' > '"+resPath+"' &";
+
+        if(!std::filesystem::exists(fPath))
+        {
+            std::ofstream output(fPath);
+            output << L"Data does not exits";
+            return task_uuid;
+        }
+
+        system(cmd0.c_str());
+    }
+    return task_uuid;
 }
