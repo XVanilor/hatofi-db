812b3c50d6785f1cc415637344b90a881cd5de53
XVanilor <vanilor.dev@gmail.com>
2023-01-20 00:25:32 +0100

    Added log warn

--- src/Log.cpp
+++ src/Log.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <string>
+#include <iostream>
 #include "Log.h"
 
 void log_info(const std::string& msg)
@@ -10,7 +11,12 @@ void log_info(const std::string& msg)
     printf("[INFO] %s\n", msg.c_str());
 }
 
+void log_warn(const std::string& msg)
+{
+    std::cerr << "[WARN] " << msg << "\n";
+}
+
 void log_error(const std::string& msg)
 {
-    printf("[ERROR] %s\n", msg.c_str());
+    std::cerr << "[ERROR] " << msg << "\n";
 }
