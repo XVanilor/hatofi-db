ab3d14d54b4757a10418f5df3954d9084ec0ae44
XVanilor <vanilor.dev@gmail.com>
2023-01-23 11:04:33 +0100

    Bug fix on UUID validation

--- src/HaUtils.cpp
+++ src/HaUtils.cpp
@@ -110,6 +110,6 @@ bool is_number(const std::string& s)
 
 bool is_valid_uuid(const std::string& s)
 {
-    static const std::regex e("[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89aAbB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}");
+    static const std::regex e("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
     return regex_match(s, e);
 }
